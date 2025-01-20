d5699dd04b8a68f2a376bd3234efe53ecdf2f8d9
Syed Mubashir Abidi <124605425+Mubashirabidi79@users.noreply.github.com>
2025-01-20 14:35:03 +0000

    first program basics

---
+++ 01_basics/test1.js
@@ -0,0 +1,17 @@
+const accountID = 14242
+let accountEmail = "mubashir@gmail.com"
+var accountPassword = 123456
+accountCity = "Karachi"
+
+// accountID = 1232 // const type cannot be changed 
+accountEmail = "jab@gmail.com" // let can be changed
+accountPassword = 98765  // var can be changed  
+accountCity = "Lahore"  // no assignment of data type can be changed 
+
+console.log(accountID)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+
+
